80ad2ff6ed169d7c7cd89cd7cf041db39b8e379b
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-10 19:07:04 +0530

    component and data binding

---
+++ 04components_and_data_binding/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = '04components_and_data_binding';
+
+  serverElements : any=[];
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+}
