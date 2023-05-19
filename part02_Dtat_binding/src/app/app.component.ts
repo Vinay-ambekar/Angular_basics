c0190b9460d3a1259055b5f245877f88b73b1fdf
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-19 12:25:15 +0530

    data binding

--- part02_Dtat_binding/src/app/app.component.ts
+++ part02_Dtat_binding/src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'part02_Dtat_binding';
+  message:string="i am string interpolation "
+  imgurl:string='https://wallpapercave.com/dwp1x/wp2637598.jpg'
+  bool:boolean=true
+  buttonclick(){
+    console.log('button is clicked')
+  }
+/*   onKeyup($event:any){
+  /*   console.log($event.keyCode) */
+  /* traditional method to enter the key */
+   /*  if($event.keyCode == 13){
+      console.log('enter key pressed')
+    } 
+  } */
+
+/* onKeyup(){
+  console.log('enter key is pressed')
+} */
+/* onKeyup($event:any){
+
+console.log($event.target.value);
+
+}  */
+onKeyup(username:any){
+
+  console.log(username);
+  
+  }
+  userName01:string='';
+
+  twowaykeyup(){
+    console.log(this.userName01)
+  }
+
 }
