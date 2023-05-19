78222023f2c9ec3e50dfaad14e1a3e1c55ac2a76
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-19 21:25:10 +0530

    'task'

--- part03_data_binding_tasks/src/app/app.component.ts
+++ part03_data_binding_tasks/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'part03_data_binding_tasks';
+  postTitle:string='';
+  postDetails:string='';
+  imageURL:string='Enter the image url';
+  postURL:string='Enter the url';
+  addBackground:boolean=false;
+  
 }
