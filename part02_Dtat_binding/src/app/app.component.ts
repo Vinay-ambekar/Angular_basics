b6358e8d447e67497e8746f3c3db5bcaaa3ed56c
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-19 08:06:58 +0530

    components and data binding

---
+++ part02_Dtat_binding/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'part02_Dtat_binding';
+}
