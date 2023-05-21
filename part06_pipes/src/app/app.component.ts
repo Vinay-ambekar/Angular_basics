d0f6249c516fb9fb16545fe5db0a103e159ce966
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-21 20:16:35 +0530

    'pipes'

--- part06_pipes/src/app/app.component.ts
+++ part06_pipes/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'part06_pipes';
+  count:number=285645;
+  dcvalue:number=3.2554554;
+  price:number=99.99
+  today:Date=new Date();
+  postobj:object={
+    id:1,
+    postTitel:'post'
+  }
+  postArray:Array<string>=[
+      'post 1',
+      'post 2',
+      'post 3',
+      'post 4',
+      'post 5',
+      'post 6',
+      'post 7',
+      'post 8',
+    ]
+userdetails={
+  name:'user 1',
+  city:'newyork',
+  countryCode:'us'
+
+}
+      
+
 }
