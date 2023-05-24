b4ad8d48ab67011638f85b6241367e69d111fd7f
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-24 11:43:21 +0530

    'custom-validation'

---
+++ part12_adding_custom_validation_Reactive_form/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder,Validators} from '@angular/forms';
+import { nospace } from './validators/nospace.validators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'part12_adding_custom_validation_Reactive_form';
+  form:any;
+  constructor(fb:FormBuilder){
+    this.form=fb.group({
+      username:['', [
+        Validators.required,
+        Validators.minLength(5),
+        nospace.nospaceValidations
+      ]],
+      password:['',[Validators.required]]
+
+    })
+}
+
+get fc(){
+  return this.form.controls;
+}
+
+
+
+
+}
