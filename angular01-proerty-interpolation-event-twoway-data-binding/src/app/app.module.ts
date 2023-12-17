7dd9996b8ad3612da6e794d59855b7158764b6d9
vinay <vinayambekar.b@gmail.com>
2023-12-17 18:15:52 +0530

    'eventbinding-and-ngmodel-that-is-two-way-data-biniding'

--- angular01-proerty-interpolation-event-twoway-data-binding/src/app/app.module.ts
+++ angular01-proerty-interpolation-event-twoway-data-binding/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,8 @@ import { TestComponent } from './test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular01-proerty-interpolation-event-twoway-data-binding/src/app/test/test.component.ts
+++ angular01-proerty-interpolation-event-twoway-data-binding/src/app/test/test.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-test',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class TestComponent {
 headding:string="hello world";
+number_of_porducts:number=0;
+serch_text:string="text is here"
 
 product={
   name:'iphone',
@@ -14,11 +17,26 @@ product={
   color:'black',
   discount:7,
   instock:6,
-  image:'https://imageio.forbes.com/specials-images/imageserve/64e74ad803781abed13b0612/Apple--iPhone--iPhone-15--iPhone-15-Pro--iPhone-15-Pro-Max--iPhone-15-release--new/0x0.jpg?format=jpg&crop=1275,956,x113,y0,safe&width=960'
+  image:'https://imageio.forbes.com/specials-images/imageserve/64e74ad803781abed13b0612/Apple--iPhone--iPhone-15--iPhone-15-Pro--iPhone-15-Pro-Max--iPhone-15-release--new/0x0.jpg?format=jpg&crop=1275,956,x113,y0,safe&width=960',
+  firstname:"firstname: ",
+  output:"out put"
 }
+
 discount(){/* is methond name with discount*/
 return this.product.price-(this.product.price*this.product.discount/100)
 }
-
+inputchange(e:any){/* this is method name with inputchange */
+return this.product.output=e.target.value
+}
+decriment(){
+if(this.number_of_porducts>0){
+  this.number_of_porducts--
+}
+}
+increment(){
+if( this.number_of_porducts<this.product.instock){
+this.number_of_porducts++
+}
+}
 
 }
